da5d5b66e6df2f18ac0eae5baa6e44383689799c
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2013-12-17 21:25:00 +0100

    Root path is pointing to cars#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Wypozyczalnia::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root :to => 'cars#index'
 
   resources :cars do
     resources :reservations
