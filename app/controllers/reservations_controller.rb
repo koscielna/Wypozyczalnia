114ca485fbab24ab27f352f3de173d94a0732927
Aleksandra Nabożny <aleksandra.nabozny@gmail.com>
2013-12-17 23:26:56 +0100

    Reservation logic included

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,6 +3,27 @@ class ReservationsController < ApplicationController
     car = Car.find(params[:car_id])
     reservation = car.reservations.create(reservation_params)
 
+    if !reservation.valid?
+      flash[:error] = reservation.errors.full_messages.join("<br>").html_safe
+    end
+
+    # raise Exception
+    # if reservation.begin_date < reservation.end_date
+    #   redirect_to car
+    # end
+
+    # car.reservations.each do |r|
+
+    #   if (reservation.begin_date < r.begin_date && reservation.end_date < r.begin_date) ||
+    #    (reservation.begin_date > r.end_date && reservation.end_date > r.end_date)
+    #     create = true
+    #   end
+    # end
+
+    # if create
+    #  reservation = car.reservations.create(reservation_params)
+    # end
+
     redirect_to car
   end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,22 @@
 class Reservation < ActiveRecord::Base
   belongs_to :car
+
+  validates_presence_of :username
+  validate :available_reservation, on: :create
+
+
+  def available_reservation
+    if begin_date > end_date
+      errors.add(:begin_date, 'Data rozpoczęcia musi nastąpić przed datą zakończenia!')
+    end
+
+    car.reservations.each do |r|
+      unless (begin_date < r.begin_date && end_date < r.begin_date) || (begin_date > r.end_date && end_date > r.end_date)
+        errors.add('Zakres dat', '- nie można dokonać rezerwacji w danym terminie.')
+        break
+      end
+    end
+
+  end
+
 end
